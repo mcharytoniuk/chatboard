7a938d712ed3c9a43a39773cfa1eaba747f899c1
Dariusz Sikorski <dariuszsikorski.pl@gmail.com>
2015-05-27 18:53:44 +0200

    Chatboard beta version commited

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+$(function(){
+
+  // chat page - calc fluid height layout
+  setInterval(function(){
+    var
+      windowHeight = $(window).innerHeight(),
+      boardHeaderHeight = $(".boardHeader").innerHeight(),
+      boardFooterHeight = $(".boardFooter").innerHeight(),
+      boardMessagesHeight = windowHeight - boardHeaderHeight - boardFooterHeight;
+      console.log("et");
+    $(".boardMessages").innerHeight(boardMessagesHeight);
+  }, 50);
+
+  // autoselect searchbox content when clicked
+  $(".search-container input").on("click", function () {
+    $(this).select();
+  });
+
+  // autoselect link when clicked
+  $(".link-container input").on("click", function () {
+    $(this).select();
+  });
+
+  // submenu panel togglers
+  $("[for-panel]").click(function(){
+    var target = $(this).attr("for-panel");
+    $(".gui-slidePanel").removeClass("active");
+    $(target).addClass("active").slideDown();
+
+    // temporarily hide bottom bar
+    $(".boardFooter").height(0);
+  });
+
+  // auto close panel when user clicks outside
+  $(document).mouseup(function (e){
+    var container = $(".gui-slidePanel.active");
+    if (!container.is(e.target)
+    && container.has(e.target).length === 0) {
+      container.removeClass("active");
+      
+      // restore bottom bar
+      $(".boardFooter").css("height","auto");
+    }
+  });
+
+
+});
