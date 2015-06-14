7286011360422eb7e7cdf65f37361fca56fc5ca5
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2015-06-14 21:56:14 +0200

    add csswring to the css build process

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@
 
 var path = require("path"),
     autoprefixer = require("autoprefixer-core"),
+    csswring = require("csswring"),
     gg = require("gore-gulp"),
     gulp = require("gulp"),
     postcss = require("gulp-postcss"),
@@ -33,14 +34,15 @@ gulp.task("fonts", ["font-awesome", "ionicons-pre"]);
 gulp.task("css", ["fonts"], function () {
     return gulp.src(path.resolve(__dirname, "assets", "scss", "style.scss"))
         .pipe(postcss([
+            postcssImport(),
+            postcssMixins(),
+            postcssNested(),
             autoprefixer({
                 "browsers": [
                     "last 2 versions"
                 ]
             }),
-            postcssImport(),
-            postcssMixins(),
-            postcssNested()
+            csswring()
         ]))
         .pipe(rename({
             "extname": ".css"
